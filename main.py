5fcb32f5a8984ceacbbca7d5325aab4193470497
VitorHornung <vitorhornung@gmail.com>
2025-04-03 15:39:16 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,12 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+# http://127.0.0.1:8000/
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+# http://127.0.0.1:8000/teste1
+@app.get("/teste1")
+async def funcaoteste():
+    return {"teste": "deu certo"}
